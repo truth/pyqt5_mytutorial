93223dc1d03d2bec6698642a869b2ff2810ee49c
wzl <truthwzl@sina.com>
2022-10-13 16:51:46 +0800

    modified release.bat xcopy the qt_material.

--- release.bat
+++ release.bat
@@ -1,3 +1,5 @@
 call .\venv\Scripts\activate.bat
 fbs freeze
 copy venv\Lib\site-packages\cv2\opencv_ffmpeg410_64.dll .\target\MyFbsApp
+xcopy venv\Lib\site-packages\qt_material .\target\MyFbsApp /E
+pause
